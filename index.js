4ed24552dca04e6ec5823c076f1b058838f74adb
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2019-05-03 09:27:32 -0300

    Chat V1.0.2
    Mudando a forma como se encontra o arquivo index.html, para funcionar corretamente no Replit.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('/index.html', {root:'.'});
 });
 
 io.on('connection', function(socket){
