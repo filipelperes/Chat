7f01dcc63d8040951547bca704904d168a0fbbb0
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2019-05-03 09:22:40 -0300

    Chat V1.0.1
    Adicionado uma mensagem quando o usuário sai ou entra no chat.

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  //Quando alguém entra no site, esse evento
+  //é acionado
+  io.emit('new person');
+
+  //Se algum cliente é desconectado do servidor
+  //é ativado esse evento
+  socket.on('disconnect', (reason) => {
+    io.emit('close person')
+  });
+
+  //Se alguma nova mensagem é escrita, esse evento
+  //vai ser ligado
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
