64c0fc69f771c00b4bcb7b811b77d0b185b97687
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2019-05-04 23:05:19 -0300

    Chat V1.0.2
    Adicionado a biblioteca Moment e modificando o evento de envio de mensagem, como também uma prévia de armazenamento de nomes no servidor.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var moment = require('moment');
+var persons = [];
 
 app.get('/', function(req, res){
   res.sendFile('/index.html', {root:'.'});
@@ -20,9 +22,14 @@ io.on('connection', function(socket){
 
   //Se alguma nova mensagem é escrita, esse evento
   //vai ser ligado
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(msg, name){
+    io.emit('chat message', name + ": " + msg + "   " + moment().get('hour') + ":" + moment().get('minute'));
   });
+
+  socket.on('addPerson', function(name){
+    persons.push(name);
+    console.log(persons);
+  })
 });
 
 http.listen(port, function(){
