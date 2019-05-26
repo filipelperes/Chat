eda65d36eca8bcbbc631c3225e410a19a86095ed
vinicius3333 <35425928+vinicius3333@users.noreply.github.com>
2019-05-26 18:53:56 -0300

    Chat V1.4.0
    Melhoria nos códigos e não se pode mais mandar mensagens sem conteúdo.

--- client/main.js
+++ client/main.js
@@ -1,48 +1,43 @@
-var moment = moment();
+var moment = moment(); // Váriavel para saber o tempo atual
 
 $(function () {
-    var TYPING_TIMER_LENGTH = 400; // ms
-    
-    var socket = io();
+    var socket = io(); // Váriavel para poder usar o Socket entre o Cliente e Servidor
 
-    var lastTypingTime;
+    var name = ''; // Nome do Cliente
+    var img; // Objeto da Imagem
 
-    var name = '';
-    var typing = false;
-    var img;
+    var $modal       = $('#myModal'); // Tela de inserção do usuário para acessar o site
+    var $formMsg     = $('#formMsg'); // Formulário que engloba o input, botão e o botão da imagem
+    var $formUser    = $('#formUser'); // Formulário que engloba o input e o botão
+    var $uploadImage = $('.upload-options'); // Botão da imagem
+    var $message     = $('#message'); // Lista de mensagens
+    var $group       = $('#group'); // Lista das pessoas online
+    var $cardChat    = $('#card-chat'); // Card onde engloba todo o escopo de mensagens e inserção de nova mensagem
+    var $cardBody    = $('#card-msg'); // Card que contém apenas as mensagens
+    var $window      = $( window ); // Tela do site
 
-    var $modal = $('#myModal');
-    var $btnName = $('#btnName');
-    var $formMsg = $('#formMsg');
-    var $sendButton = $('#sendButton');
-    var $sendImage = $('#sendImage');
-    var $formUser = $('#formUser');
-    var $uploadImage = $('.upload-options');
-    var $inputName = $('#inputName');
-    var $imgChat = $('#img-chat');
-    var $typing = $('#typing');
-    var $m = $('#m');
-    var $notify = $('#notify');
-    var $message = $('#message');
-    var $sent_msg = $('.sent_msg');
-    var $received_withd_msg = $('.received_withd_msg');
-    var $group = $('#group');
-    var $butt = $('#butt');
-    var $cardChat = $('#card-chat');
-    var $cardBody = $('#card-msg');
-    var $window = $( window );
+    // A tela de login é ligada
+    $modal.modal({             
+      backdrop: 'static',
+      keyboard: false,
+      focus: false,
+      show: true
+    });
 
+    // Função para quando é mudado o tamanho da tela
     function resize(){
       $cardChat.height($window.height() - 50 );
       $cardBody.height($window.height() - 50 );     
     }
 
+    // Função para registrar o nome do usuário
     function setUser(name){
       socket.emit('addPerson', name);
       $modal.modal('hide');
     }
 
-    const addMessageChat = (data, options) => {
+    // Função que insere a mensagem no corpo do html
+    function addMessageChat(data, options) {
       var msg = options.log ? data.msg : data.name + ": " + data.msg;
       if(options.image){
         if(options.right){
@@ -90,37 +85,37 @@ $(function () {
           ));
         }
       } else {
-        if(options.right){
-          $message.append($('\
-            <div class="outgoing_msg">\
-              <div class="sent_msg">\
-                <p>' + msg + '</p>\
-                <span class="time_date"> ' + data.time + '</span>\
-              </div>\
-            </div>'
-          ));
-        } else {
-          $message.append($('\
-            <div class="incoming_msg">\
-              <div class="received_msg">\
-                <div class="received_withd_msg">\
-                  <p>' + msg +  '</p>\
-                  <span class="time_date">' + data.time + '</span>\
-                </div>\
+        var right = '\
+          <div class="outgoing_msg">\
+            <div class="sent_msg">\
+              <p>' + msg + '</p>\
+              <span class="time_date"> ' + data.time + '</span>\
+            </div>\
+          </div>'
+        var left = '\
+          <div class="incoming_msg">\
+            <div class="received_msg">\
+              <div class="received_withd_msg">\
+                <p>' + msg +  '</p>\
+                <span class="time_date">' + data.time + '</span>\
               </div>\
             </div>\
-          '))
-        }
+          </div>'
+        $message.append($(
+          options.right ? right : left
+        ));
       }
 
       $cardBody.scrollTop($cardBody[0].scrollHeight);
     };
 
-    const addGroupChat = (list) => {
+    // Função para adicionar uma pessoa a lista de quem está online
+    function addGroupChat(list) {
       $group.append(list);
     }
 
-    const generateGroup = (group) => {
+    // Função para carregar as pessoas que estão online
+    function generateGroup(group) {
       var list = document.createElement('ul');
       list.setAttribute("class", "list-group list-group-flush");
 
@@ -136,34 +131,11 @@ $(function () {
       addGroupChat(list);
     }
 
-    const addTypingChat = (msg) => {
-      $typing.text(msg);
-    }
-
-    const updateTyping = (msg) => {
-      if (!typing) {
-        typing = true;
-        socket.emit('typing', name + "...");
-      }
-      lastTypingTime = (new Date()).getTime();
-
-      setTimeout(() => {
-        var typingTimer = (new Date()).getTime();
-        var timeDiff = typingTimer - lastTypingTime;
-        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-          socket.emit('stop typing');
-          typing = false;
-        }
-      }, TYPING_TIMER_LENGTH);
-      //$typing.text(msg);
-    }
-    
-    $uploadImage.on('change', getFile);
-
-    function getFile(image){
+    // Quando o botão da imagem é acionado, esse evento é chamado
+    $uploadImage.on('change', function(image) {
       img = image;
       var name = image.target.files[0].name;
-      var imgName = name.substring(name.indexOf('.'));
+      var imgName = name.substring(name.lastIndexOf('.'));
 
       $('.upload-options label').addClass('without-img');
       if($('.without-img p').length) {
@@ -174,16 +146,10 @@ $(function () {
           <p style="position: absolute; left: 18%; top: 22%;">' + imgName + '</p>\
         ')
       )
-    }
-
-    $modal.modal({
-      backdrop: 'static',
-      keyboard: false,
-      focus: false,
-      show: true
     });
 
-    $formUser.submit( function(e){
+    // Quando ou o botão ou a tecla Enter são acionados, esse evento é chamado na tela de login
+    $formUser.submit(function(e){
       if(e.currentTarget[0].value != '') {
         name = e.currentTarget[0].value;
         setUser(name);
@@ -191,83 +157,57 @@ $(function () {
       return false;
     });
 
-    $sendButton.on('click', function(){
-      if(img){
-        console.log('tem alguma imagem')
-        var msg = $m.val();
-        var file = img.target.files[0];
+    // Quando ou o botão ou a tecla Enter são acionados, esse evento é chamado na tela principal
+    $formMsg.on('submit', function(e){
+      var msg = e.delegateTarget[1].value;
+      if(msg.length > 0) {
+        var file = img ? img.target.files[0] : undefined;
         var data = {
           time: moment.get('hour') + ":" + moment.get('minute'),
           msg: msg,
           name: name,
           image: file
-        } 
+        }
         addMessageChat(data, {
           right: true,
           log: false,
-          image: true
+          image: img ? true : false
         });
         socket.emit('chat message', data, {
           right: false,
           log: false,
-          image: true
+          image: img ? true : false
         });
         $('.without-img p').remove();
         $('.upload-options label').removeClass('without-img');
         img = undefined;
-      } else{
-        console.log('não tem alguma imagem');
-        var msg = $m.val();
-        var data = {
-          time: moment.get('hour') + ":" + moment.get('minute'),
-          msg: msg,
-          name: name
-        }
-        console.log(data)
-        addMessageChat(data, {
-          right: true,
-          log: false,
-          image: false
-        });
-        socket.emit('chat message', data, {
-          right: false,
-          log: false,
-          image: false
-        });
-        $m.val('');
+
+        e.delegateTarget[1].value = '';
       }
       return false;
-    });
-
-    $sendImage.on('click', function(){
-      console.log('imagem')
-      return false
     })
 
-    $formMsg.on('input', () => {
-      updateTyping();
-    })
-
+    // Quando se clica em alguma imagem no chat, esse evento é chamado
     $message.on('click', 'a', function(){
 			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
       $('#imagemodal').modal('show'); 
       return false;  
     })
 
+    // Esse evento faz com que as mensagens sejão escritas para todos os usuários menos o próprio
+    // que enviou
     socket.on('chat message', function(data, options){
-      console.log(options);
-      if(options.image){
-        addMessageChat(data, options);
-      } else {
-        addMessageChat(data, options);
-      }
+      addMessageChat(data, options);
     })
 
+    // Quando algum novo usuário entra, essa função é chamada para o próprio usuário, contendo
+    // todas as pessoas online atualmente e mostrando uma mensagem 
     socket.on('welcomeMessage', function(users){
       addMessageChat({
         msg: 'Bem vindo ' + name + '!',
         time: moment.get('hour') + ":" + moment.get('minute')
-      }, { 
+      }, 
+      { 
         right: false,
         log: true
       })
@@ -275,31 +215,24 @@ $(function () {
       $message.scrollTop($message.height());
     })
 
+    // Quando algum novo usuário entra, essa função é chamada para todos online, menos o próprio
+    // que entrou, adicionando na lista de pessoas online e mostrando uma mensagem
     socket.on('addPerson', (name, users) => {
       addMessageChat({  
         msg: name + ' acabou de entrar na sala!',
         time: moment.get('hour') + ":" + moment.get('minute')
-      }, { 
+      }, 
+      { 
         right: false,
         log: true
       })
-      console.log(users);
-      console.log($group);
       $group[0].removeChild($group[0].childNodes[1]);
       generateGroup(users);
       $message.scrollTop($message.height());
     })
 
-    socket.on('typing', (msg) => {
-      addTypingChat(msg);
-      $message.scrollTop($message.height());
-    })
-
-    socket.on('stop typing', () => {
-      addTypingChat('');
-      $message.scrollTop($message.height());
-    })
-
+    // Quando algum novo usuário entra, essa função é chamada para todos online, menos o próprio
+    // que entrou, removendo o usuário de pessoas online e mostrando uma mensagem
     socket.on('close person', (name, users) => {
       addMessageChat({  
         msg: name + ' saiu da sala!',
@@ -313,9 +246,11 @@ $(function () {
       $message.scrollTop($message.height());
     })
 
+    // Quando a tela mudar de tamanho essa função será chamada
     $window.resize(function() {
       resize();
     });
 
+    // Ajustar a tela pela primeira vez
     resize();
 });
--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
-var port = process.env.PORT || 3000;
+var express = require('express'); // Flamework do Servidor
+var app     = express(); // Váriavel do express
+var http    = require('http').Server(app); // Servidor em si
+var io      = require('socket.io')(http); // Socket io usando o servidor
+var path    = require('path'); // Módulo para manipular caminhos
+var port    = process.env.PORT || 3000; // Porta do servidor
 
-app.use(express.static(path.join(__dirname, 'client')));
+// Mandando o Express rodar estáticamente a pasta client
+app.use(express.static(path.join(__dirname, 'client'))); 
 
+// Váriavel que armazena as pessoas online
 var users = [];
 
+// Na rota principal, a página é carregada 
 app.get('/', function(req, res){
   res.sendFile('/client/index.html', {root:'.'});
 });
 
+// Quando se tem uma conexão no servidor, esse evento é chamado
 io.on('connection', function(socket){
 
-  //Se algum cliente é desconectado do servidor
-  //é ativado esse evento
+  //Se algum cliente é desconectado do servidor, esse evento é chamado
   socket.on('disconnect', (reason) => {
     users.splice(users.indexOf(socket.name), 1);
     socket.broadcast.emit('close person', socket.name, users);
   });
 
-  //Se alguma nova mensagem é escrita, esse evento
-  //vai ser ligado
+  //Se alguma nova mensagem é enviada, esse evento é chamado
   socket.on('chat message', function(data, options){
     socket.broadcast.emit('chat message', data, options);
   });
 
-  socket.on('typing', (msg) => {
-    socket.broadcast.emit('typing', msg);
-  })
-
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing');
-  })
-
+  // Quando alguém entra na sala, esse evento é chamado
   socket.on('addPerson', function(name){
     socket.name = name;
     users.push(name);
@@ -46,6 +41,5 @@ io.on('connection', function(socket){
   })
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+// Rodando o servidor na determinada porta
+http.listen(port);
